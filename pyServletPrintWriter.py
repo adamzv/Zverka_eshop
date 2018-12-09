ac33e8914b450d213dc3b1f754b89614f623e10e
Adam Zverka <adamzverka@hotmail.com>
2018-12-09 18:36:12 +0100

    Skladanie layoutu

---
+++ pyServletPrintWriter.py
@@ -0,0 +1,63 @@
+import argparse
+import sys
+from pathlib import Path
+from datetime import datetime
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Spracuje html súbor do podoby využiteľnej v servletoch.\n"
+                                                 "napr. out.println(\"<h1>Java PrintWriter</h1>\");")
+    parser.add_argument("subor", metavar="subor", nargs=1, help="súbor, ktorého dáta sa majú spracovať")
+    const = "pyspw_" + datetime.strftime(datetime.now(), "%Y%m%d%H%M%S") + ".html"
+    parser.add_argument("-w", nargs="?", const=const, dest="zapis", required=False, help="voliteľný súbor, do ktorého sa majú zapísať spracované dáta")
+
+    return parser
+
+
+def spracuj(vstupny_subor, vystupny_subor=None):
+    subor_na_citanie = Path(vstupny_subor)
+
+    # ak sa vstupný súbor nenašiel, ukonči spracovanie
+    if not subor_na_citanie.is_file():
+        print("Súbor na čítanie sa nenašiel")
+        return False
+    else:
+        try:
+            vstup = open(vstupny_subor, "r", encoding="utf8")
+            vystup = open(vystupny_subor, "a", encoding="utf8")
+
+        # Ak sa pri vstupe nezadá argument -w, tak hodnota atribútu args.zapis bude None
+        except TypeError:
+            vystup = None
+        finally:
+            for line in enumerate(vstup):
+                line = line[1].rstrip()
+                if line == "":
+                    line = "out.println();"
+                else:
+                    line = line.replace("\"", "\\\"")
+                    line = f"out.println(\"{line}\");"
+                if vystup is not None:
+                    vystup.write(line + "\n")
+                else:
+                    print(line)
+
+            vstup.close()
+            if vystup is not None:
+                vystup.close()
+        return True
+
+
+
+
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    print(args)
+    spracuj(args.subor[0], args.zapis)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/java/zverka_eshop/Layout.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zverka_eshop;
+
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author adamzv
+ */
+public class Layout {
+
+    public static final int ZACIATOK_HTML = 0;
+    public static final int KONIEC_HTML = 1;
+
+    private static final String TITLE = "";
+
+    public static void vypis_html(int mod, PrintWriter out) {
+        private_vypis_html(mod, out, TITLE);
+    }
+
+    public static void vypis_html(int mod, PrintWriter out, String title) {
+        private_vypis_html(mod, out, title);
+    }
+
+    public static void vypis_navbar(PrintWriter out) {
+        out.println("<nav class=\"navbar navbar-expand-md navbar-light bg-light border\">");
+        out.println("    <a class=\"navbar-brand\" href=\"/\">jShop Zverka</a>");
+        out.println("    <button aria-controls=\"navbar\" aria-expanded=\"false\" aria-label=\"Toggle navigation\" class=\"navbar-toggler\"");
+        out.println("            data-target=\"#navbar\" data-toggle=\"collapse\" type=\"button\">");
+        out.println("        <span class=\"navbar-toggler-icon\"></span>");
+        out.println("    </button>");
+        out.println("    <div class=\"collapse navbar-collapse\" id=\"navbar\">");
+        out.println("            <ul class=\"navbar-nav mr-auto mt-2\">");
+        out.println("                <li class=\"nav-item\"><a class=\"nav-link\" href=\"/ponuka\">Ponuka</a></li>");
+        out.println("                <li class=\"nav-item\"><a class=\"nav-link\" href=\"/kosik\">Košík</a></li>");
+        out.println("                <li class=\"nav-item\"><a class=\"nav-link\" href=\"/objednavky\">Objednávky</a></li>");
+        out.println("            </ul>");
+        out.println();
+        out.println("            <ul class=\"navbar-nav ml-auto mt-2\">");
+        out.println("                <li class=\"nav-item\"><a class=\"nav-link\" href=\"/login\">Prihlásiť sa</a></li>");
+        out.println("            </ul>");
+        out.println("    </div>");
+        out.println("</nav>");
+    }
+
+    public static void vypis_footer(PrintWriter out) {
+        out.println("<footer class=\"small text-center text-muted\">");
+        out.println("    jShop Zverka 2018 (KI/OT/15)");
+        out.println("</footer>");
+    }
+
+    private static void private_vypis_html(int mod, PrintWriter out, String title) {
+        switch (mod) {
+            case 0:
+                out.println("<!DOCTYPE html>");
+                out.println("<html lang=\"sk\">");
+                out.println("<head>");
+                out.println("    <meta charset=\"utf-8\"/>");
+                out.println("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"/>");
+                out.println();
+                out.println("    <link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css\" rel=\"stylesheet\"/>");
+                out.println("    <link href=\"/static/styles.css\" rel=\"stylesheet\"/>");
+                out.println();
+                out.println("    <script src=\"https://code.jquery.com/jquery-3.1.1.min.js\"></script>");
+                out.println("    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js\"></script>");
+                out.println("    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js\"></script>");
+                out.println("    <title>" + title + "</title>");
+                out.println("</head>");
+                out.println();
+                out.println("<body>");
+                break;
+            case 1:
+                out.println("</body>");
+                out.println("</html>");
+                break;
+        }
+    }
+}
